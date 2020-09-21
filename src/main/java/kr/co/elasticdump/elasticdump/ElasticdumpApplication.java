37bfec6273020b28d296135d5fc1a690f76e2bf2
Milky <tpdhks0369@gmail.com>
2020-09-21 21:05:22 +0900

    init : 프로젝트 최초 커밋

---
+++ src/main/java/kr/co/elasticdump/elasticdump/ElasticdumpApplication.java
@@ -0,0 +1,13 @@
+package kr.co.elasticdump.elasticdump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticdumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticdumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/elasticdump/elasticdump/ElasticdumpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.elasticdump.elasticdump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticdumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
